53a94e139784bf1b20cbba22cb1d8c1ac95e3245
Harikrushna <32766262+hardikhari96@users.noreply.github.com>
2023-09-24 00:29:59 +0530

    sms read cr implimented

--- app/src/main/java/turiya/digitals/commushield/MainActivity.kt
+++ app/src/main/java/turiya/digitals/commushield/MainActivity.kt
@@ -1,16 +1,14 @@
 package turiya.digitals.commushield
 
-import android.Manifest.permission.READ_SMS
 import android.Manifest.permission.READ_CALL_LOG
+import android.Manifest.permission.READ_SMS
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Telephony
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-
-
-
-
 
 class MainActivity : AppCompatActivity() {
     private val permissions =  ArrayList<String>()
@@ -24,10 +22,33 @@ class MainActivity : AppCompatActivity() {
         permissions.add(READ_SMS)
         permissions.add(READ_CALL_LOG)
         permissionsToRequest = findUnAskedPermissions(permissions)
-        requestPermissions(
-            (permissionsToRequest.toTypedArray<String?>())!!,
-            ALL_PERMISSIONS_RESULT
-        )
+//        requestPermissions(
+//            (permissionsToRequest.toTypedArray<String?>())!!,
+//            ALL_PERMISSIONS_RESULT
+//        )
+
+        val inboxURI = Uri.parse("content://sms/inbox")
+        val reqCols = arrayOf("_id", "address", "body")
+        val cr = contentResolver
+        val c = cr.query(inboxURI, reqCols, null, null, null)
+        if (c != null) {
+            try {
+                if (c.moveToFirst()) {
+                    val senderColumnIndex = c.getColumnIndexOrThrow(Telephony.Sms.Inbox.ADDRESS)
+                    val messageColumnIndex = c.getColumnIndexOrThrow(Telephony.Sms.Inbox.BODY)
+
+                    do {
+                        val sender = c.getString(senderColumnIndex)
+                        val message = c.getString(messageColumnIndex)
+
+                        // Log the message details
+                        Log.d("SmsReader", "Sender: $sender, Message: $message")
+                    } while (c.moveToNext())
+                }
+            } finally {
+                c.close() // Close the cursor when you're done with it
+            }
+        }
     }
 
     private fun findUnAskedPermissions(wanted: ArrayList<String>): ArrayList<String> {
@@ -50,4 +71,5 @@ class MainActivity : AppCompatActivity() {
     private fun canMakeSmores(): Boolean {
         return Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1
     }
+
 }
